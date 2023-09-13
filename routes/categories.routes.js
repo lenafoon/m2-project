b4e574d07cbf42d151a0e3abbbd85ddbf5c83917
Lena Foon <lena.foon@gmail.com>
2023-09-13 19:47:23 +0200

    more CSS

--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -4,5 +4,16 @@ const router = require('express').Router();
 //GET
 router.get('/categories', (req, res) => res.render('categories'));
 
+router.get('/challenges', (req, res) => res.render('challenges'));
+
+router.get('/health', (req, res) => res.render('health'));
+
+router.get('/home', (req, res) => res.render('home'));
+
+router.get('/personal', (req, res) => res.render('personal'));
+
+router.get('/shopping', (req, res) => res.render('shopping'));
+
+router.get('/work', (req, res) => res.render('work'));
 
 module.exports = router;
