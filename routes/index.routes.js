7059e99a2bc2c3fad8ef24f910750bc9e22d50e2
MeenaThirunaukkarasu <134803165+MeenaThirunaukkarasu@users.noreply.github.com>
2023-09-27 11:01:19 +0200

    task count in dashboard fixed

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const { categories } = require('../public/js/categories');
 const router = express.Router();
+const Task = require('./../models/Task.model')
+const categoriesArray=[...categories]
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index", { isLoggedIn: true , categories});
+  categoriesArray.forEach(categories=>{
+    Task.find({category: categories.name})
+    .then(tasks=>{
+      categories.length=tasks.length
+    })
+    .catch(error=>{
+      console.log("error while finding task",error)
+    })
+  })
+
+  console.log(categoriesArray)
+  res.render("index", { isLoggedIn: true , categories:categoriesArray});
+
+
+
+
 });
 
 module.exports = router;
